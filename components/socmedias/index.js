ace047ba8dc7d8e29729d41a6a5b7035aa1ee7b5
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-08-25 19:06:12 +0400

    expressions is written for dynamic layout

--- components/socmedias/index.js
+++ components/socmedias/index.js
@@ -1,18 +1,26 @@
 import React from "react";
 import Contact from "./../contact/index";
 import { CONTACTS } from "./../../constants/index";
+import useWindowSize from "./../../hooks/useWindowSize";
+import { Burger } from "../icons";
 
 import cn from "classnames";
 
 import styles from "./socmedias.module.css";
 
 const SocialMedias = ({ className, ...props }) => {
-  return (
+  const size = useWindowSize();
+
+  return size.width > 880 ? (
     <nav className={cn(styles.contacts, className)} {...props}>
       {CONTACTS.map((contact) => {
         return <Contact key={contact.key}>{contact.icon}</Contact>;
       })}
     </nav>
+  ) : (
+    <Contact className={styles.burger}>
+      <Burger />
+    </Contact>
   );
 };
 
