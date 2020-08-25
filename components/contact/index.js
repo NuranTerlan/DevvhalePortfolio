ad13a0f6930a8469add78d2b585054054b2d2c98
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-08-25 19:03:08 +0400

    button created

--- components/contact/index.js
+++ components/contact/index.js
@@ -5,9 +5,9 @@ import styles from "./contact.module.css";
 
 const Contact = ({ children, className, ...props }) => {
   return (
-    <div className={cn(styles.button, className)} {...props}>
+    <button className={cn(styles.button, className)} {...props}>
       {children}
-    </div>
+    </button>
   );
 };
 
