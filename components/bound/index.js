638acc24e02922faa37f99f5081c73ca2508e9fb
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-08-25 13:15:42 +0400

    Line Component to seperate each Section is created

---
+++ components/bound/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import cn from "classnames";
+
+import styles from "./bound.module.css";
+import { ChevronDown } from "../icons";
+
+const Bound = ({ isDown = false, className }) => {
+  return (
+    <div className={cn(styles.container, className)}>
+      {isDown ? (
+        <span className={styles.down}>
+          <ChevronDown />
+        </span>
+      ) : null}
+      <div className={styles.line}></div>
+    </div>
+  );
+};
+
+export default Bound;
