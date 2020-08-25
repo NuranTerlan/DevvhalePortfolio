ceb23822143963e8b6e8b9b452db715f913a7171
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-08-25 13:18:08 +0400

    Footer created

--- components/footer/index.js
+++ components/footer/index.js
@@ -3,7 +3,13 @@ import React from "react";
 import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>Footer here !!</div>;
+  return (
+    <footer className={styles.footer}>
+      <h1>
+        Nuran Terlan Design | <span className="theme">DEVVHALE</span> 2020
+      </h1>
+    </footer>
+  );
 };
 
 export default Footer;
