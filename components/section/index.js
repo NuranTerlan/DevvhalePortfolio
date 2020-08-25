8c4c7c844c5a7a7969bdb1b1ba3c5e63b859c649
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-08-25 13:20:42 +0400

    Section component is created for clean architecture and semantic layout

---
+++ components/section/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import cn from "classnames";
+
+import styles from "./section.module.css";
+
+const Section = ({ children, className, ...props }) => {
+  return (
+    <section className={cn(styles.section, className)} {...props}>
+      {children}
+    </section>
+  );
+};
+
+export default Section;
