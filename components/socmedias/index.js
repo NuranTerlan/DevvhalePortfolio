8dda7f84f28b0e7ae89b565bd69f38004e661ec0
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-08-26 23:23:36 +0400

    toggle button added

--- components/socmedias/index.js
+++ components/socmedias/index.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Contact from "./../contact/index";
 import { CONTACTS } from "./../../constants/index";
 import useWindowSize from "./../../hooks/useWindowSize";
-import { Burger } from "../icons";
+import { Burger, Cancel } from "../icons/index";
 
 import cn from "classnames";
 
@@ -10,6 +10,24 @@ import styles from "./socmedias.module.css";
 
 const SocialMedias = ({ className, ...props }) => {
   const size = useWindowSize();
+
+  const [open, setOpen] = useState(false);
+
+  const toggleMediaModal = () => {
+    const layer = document.getElementById("mediaLayer");
+
+    if (open === true) {
+      layer.style.visibility = "hidden";
+      layer.style.opacity = "0";
+    }
+
+    if (open === false) {
+      layer.style.visibility = "visible";
+      layer.style.opacity = "100%";
+    }
+
+    setOpen(!open);
+  };
 
   return size.width > 880 ? (
     <nav className={cn(styles.contacts, className)} {...props}>
@@ -24,9 +42,34 @@ const SocialMedias = ({ className, ...props }) => {
       })}
     </nav>
   ) : (
-    <Contact className={styles.burger}>
-      <Burger />
-    </Contact>
+    <>
+      <Contact className={styles.burger} onClick={() => toggleMediaModal()}>
+        <Burger />
+      </Contact>
+      <div
+        className={styles.layer}
+        id="mediaLayer"
+        onClick={() => toggleMediaModal()}
+      >
+        <div className={styles.menuModal}>
+          <h1>Social Medias</h1>
+          <div className={styles.menu}>
+            {CONTACTS.map((contact) => {
+              return (
+                <a href={contact.link} target="_blank" rel="noopener">
+                  <Contact aria-label={contact.key} key={contact.key}>
+                    {contact.icon}
+                  </Contact>
+                </a>
+              );
+            })}
+          </div>
+          <Contact className={styles.cancel} onClick={() => toggleMediaModal()}>
+            <Cancel />
+          </Contact>
+        </div>
+      </div>
+    </>
   );
 };
 
