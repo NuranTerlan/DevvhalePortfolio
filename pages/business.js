0d1292b1b4e9b5c599c470d112685a24098448e7
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-08-26 15:46:34 +0400

    business page created

---
+++ pages/business.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Head from "next/head";
+import Layout from "./../components/layout/index";
+// import useWindowSize from "./../hooks/useWindowSize";
+import Section from "./../components/section/index";
+import Bound from "./../components/bound";
+import Title from "./../components/title/index";
+import Quote from "./../components/quote/index";
+
+const Business = () => {
+  return (
+    <>
+      <Head>
+        <title>Business &sdot; DEVVHALE Contract</title>
+        <meta
+          name="description"
+          content="DEVVHALE is Freelance Full Stack Developer and in this page you can order your dream website and enjoy our special offers just for you. DEVVHALE 2020 !"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="google-site-verification"
+          content="iKC2hErCRhHDyAcM9g93KcaEYgPDTORTKjsCf6nzYas"
+        />
+      </Head>
+
+      <Layout>
+        <Section></Section>
+      </Layout>
+    </>
+  );
+};
+
+export default Business;
