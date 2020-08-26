408271b17b64db53c70f73de5f4c25a0e57dcfbd
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-08-26 15:45:45 +0400

    down icon is fixed for navigator button and menu items

--- components/navigator/index.js
+++ components/navigator/index.js
@@ -5,11 +5,11 @@ import cn from "classnames";
 
 import styles from "./navigator-button.module.css";
 
-const NavigatorButton = ({ children, className, ...props }) => {
+const NavigatorButton = ({ down = true, children, className, ...props }) => {
   return (
     <button className={cn(styles.button, className)} {...props}>
       {children}
-      <ChevronDown className={styles.downSvg} />
+      {down && <ChevronDown className={styles.downSvg} />}
     </button>
   );
 };
