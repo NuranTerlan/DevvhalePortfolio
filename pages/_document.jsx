2cd993d9ea7402c78388343ef1b9c39fe81081bc
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-08-27 22:41:05 +0400

    browser tab colors are configured for different devices

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,15 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="/static/Logo.svg" />
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="robots" content="index, follow" />
-          <meta name="googlebot" content="index" />
+          {/* Chrome, Firefox OS and Opera */}
+          <meta name="theme-color" content="#010101" />
+          {/* Windows Phone */}
+          <meta name="msapplication-navbutton-color" content="#010101" />
+          {/* iOS Safari */}
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="#010101"
+          />
         </Head>
         <body>
           <Main />
