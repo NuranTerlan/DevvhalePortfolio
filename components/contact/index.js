c0b873be67017168fecafc6c23c322d0e0dd493e
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-08-27 22:43:43 +0400

    isRef prop added contact component

--- components/contact/index.js
+++ components/contact/index.js
@@ -3,11 +3,15 @@ import cn from "classnames";
 
 import styles from "./contact.module.css";
 
-const Contact = ({ children, className, ...props }) => {
-  return (
+const Contact = ({ isRef = true, children, className, ...props }) => {
+  return isRef === true ? (
     <button className={cn(styles.button, className)} {...props}>
       {children}
     </button>
+  ) : (
+    <div className={cn(styles.button, className)} {...props}>
+      {children}
+    </div>
   );
 };
 
