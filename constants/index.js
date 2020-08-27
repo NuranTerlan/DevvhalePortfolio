5b252339e5f78e51556e12d4246601d294258d44
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-08-27 22:42:09 +0400

    Certificate, skill and hobby dummy data is added

--- constants/index.js
+++ constants/index.js
@@ -7,6 +7,16 @@ import {
   InstagramOriginal,
   GithubOriginal,
   FacebookOriginal,
+  Chess,
+  Bicycle,
+  Billard,
+  Gaming,
+  Box,
+  Meal,
+  Coding,
+  Mountains,
+  Science,
+  Music,
 } from "../components/icons/index";
 
 export const SIZE = {
@@ -14,6 +24,100 @@ export const SIZE = {
   MOBILE_SIZE: 700,
   DESK_SIZE: 1270,
 };
+
+export const CERTIFICATES = [
+  {
+    name: "SQL for Data Science",
+    issuer: "University of California, Davis",
+    credential: "UY7N2EZL9LQK",
+    source: "/static/sql-certificate.png",
+    link:
+      "https://www.coursera.org/account/accomplishments/certificate/UY7N2EZL9LQK",
+  },
+  {
+    name: "C# Scripting and Unity",
+    issuer: "University of Colorado",
+    credential: "KM6WNSM4H3XF",
+    source: "/static/unity-certificate.png",
+    link:
+      "https://www.coursera.org/account/accomplishments/certificate/KM6WNSM4H3XF",
+  },
+  {
+    name: "MTA:  HTML and CSS - Certified 2020",
+    issuer: "Microsoft",
+    credential: " 0f5b44ff-27d2-4ca9-9b7e",
+    source: "/static/html-css-certificate.png",
+    link:
+      "https://www.youracclaim.com/badges/0f5b44ff-27d2-4ca9-9b7e-a9a24381a829/linked_in_profile",
+  },
+  {
+    name: "AWS Fundamentals: Going Cloud-Native",
+    issuer: "Amazon Web Services (AWS)",
+    credential: "FARL9HRZT4YW",
+    source: "/static/aws-certificate.png",
+    link:
+      "https://www.coursera.org/account/accomplishments/certificate/FARL9HRZT4YW",
+  },
+];
+
+export const SKILLS = [
+  {
+    name: "C#",
+  },
+  {
+    name: "ASP.Net Core",
+  },
+  {
+    name: "Rest API",
+  },
+  {
+    name: "Visual Studio",
+  },
+  {
+    name: "Figma",
+  },
+  {
+    name: "HTML5",
+  },
+  {
+    name: "CSS3",
+  },
+  {
+    name: "React",
+  },
+  {
+    name: "NextJS",
+  },
+  {
+    name: "VS Code",
+  },
+  {
+    name: "JavaScript",
+  },
+  {
+    name: "MySQL",
+  },
+  {
+    name: "SQL Server",
+  },
+  {
+    name: "T-SQL",
+  },
+  {
+    name: "Github",
+  },
+  {
+    name: "Vercel",
+  },
+
+  {
+    name: "Unity",
+  },
+
+  {
+    name: "Problem Solving",
+  },
+];
 
 export const MENU = [
   {
@@ -61,5 +165,48 @@ export const CONTACTS = [
     key: "facebook",
     icon: <FacebookOriginal />,
     link: "https://www.facebook.com/devvhale/",
+  },
+];
+
+export const HOBBIES = [
+  {
+    icon: <Chess />,
+    key: "chess",
+  },
+  {
+    icon: <Bicycle />,
+    key: "bicycle",
+  },
+  {
+    icon: <Billard />,
+    key: "billard",
+  },
+  {
+    icon: <Gaming />,
+    key: "gaming",
+  },
+  {
+    icon: <Box />,
+    key: "boxing",
+  },
+  {
+    icon: <Meal />,
+    key: "chef",
+  },
+  {
+    icon: <Coding />,
+    key: "coding",
+  },
+  {
+    icon: <Mountains />,
+    key: "hiking",
+  },
+  {
+    icon: <Science />,
+    key: "learning",
+  },
+  {
+    icon: <Music />,
+    key: "music",
   },
 ];
