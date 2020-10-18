565303bc7c63bc972ead062a18402ba7250e6a2e
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2020-10-18 13:00:01 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 const FirstPage = () => {
@@ -9,7 +10,16 @@ const FirstPage = () => {
       router.push("/home");
     }
   });
-  return null;
+  return (
+    <Head>
+      <title>Welcome &sdot; DEVVHALE</title>
+        <meta
+          name="description"
+          content="This is an official Promotional Website of DEVVHALE. DEVVHALE is a new brand. Creator and Founder of that brand is Nuran Terlan from Azerbaijan !"
+        />
+        <meta name="google-site-verification" content="iKC2hErCRhHDyAcM9g93KcaEYgPDTORTKjsCf6nzYas" />
+    </Head>
+  );
 };
 
 export default FirstPage;
